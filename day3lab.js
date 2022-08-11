76889134af873bbffbfb73820b570aeb2b6dc068
Tiaja4444 <110510236+Tiaja4444@users.noreply.github.com>
2022-08-11 09:04:38 -0400

    Add files via upload

---
+++ day3lab.js
@@ -0,0 +1,38 @@
+//Write an if/else statement for the following requirements:
+// If student gets 80 or higher: console log You did a good job
+// If students get 70 or above: console log Keep trying
+// If students get 60 or above: console log ehhhh
+// If students get 55 or above: console log Not to good
+// Any grade lower than 55 is Bad Grade But You'll Get Em Next Time
+// let grade =79 
+// if (grade >= 80){
+//         console.log("you scored " + grade + " you did a good job")
+//     } else if (grade >= 70){
+//         console.log("you scored " + grade + " keep trying")
+//     } else if (grade >=60){
+//         console.log("you scored " + grade + " ehhhh")
+//     } else if (grade >=55){
+//         console.log("you scored " + grade + " not too good")
+//     }else ("you scored" + grade + " bad grade but you'll get em next time")
+
+    //Write a for loop for the given output: 10, 30, 50, 70, 90 20, 40, 60, 80, 100
+    // for (let i = 10; i<=100;i+=20){
+    //     console.log(i)
+    // }
+        // for (let i = 20; i<=100;i+=20){
+        //     console.log(i)
+        // }
+
+//Write a program that prints the numbers from 1 to 100 and for multiples 
+//of '3' print "Fizz" instead of the number and for the multiples of '5' print "Buzz". 
+//If can be multiplied by 3&5 console log 'FizzBuzz'
+for (let i = 1; i <=100; i++){
+    console.log(i)
+    if (i = 3, 6, 9){
+        console.log("fizz")
+    }
+}
+
+
+
+
